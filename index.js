6b550f9651d39d5f82051ec16e1ae4c3f6b0e2aa
Becky Lang <blang@dminc.com>
2022-08-13 16:28:24 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var itemPrice = 99.00
+var moneyInWallet = 120.00
+var numFriends = 4
+var age = 46
+var firstName = "Becky"
+var lastName = "Lang"
+var middleInitial = "J"
+
+//New amount of money in wallet after buying item
+if (moneyInWallet = moneyInWallet - itemPrice){
+console.log(`Money I have left is ` + moneyInWallet);
+}
+//Number of friends I've made each year based on my age variable & my number of friends variable
+if (averagePerYear = (numFriends / age)){
+    console.log(`My average number of friends made per year is `+ averagePerYear);
+}
+//Full name based on first name, middle initial, and last name
+if (fullName = firstName + " " + middleInitial + " " + lastName){
+
+    console.log(`You may call me ` + fullName);
+}
