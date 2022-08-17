aeaa318488bf84f17b0e1175fb1b381ba249d281
Becky Lang <blang@dminc.com>
2022-08-17 18:56:28 -0400

    second commit

--- index.js
+++ index.js
@@ -19,3 +19,67 @@ if (fullName = firstName + " " + middleInitial + " " + lastName){
 
     console.log(`You may call me ` + fullName);
 }
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(0, 1);
+console.log(fruits);
+
+let names = ["Ricky", "Tali", "Ed", "Sassy", "Margaret"]
+let lengths = names.map (function(element) {
+    return element.length;
+});
+console.log(lengths);
+
+let sum = lengths.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;
+})
+console.log(sum);
+
+names.forEach(function(element) {
+console.log(element);
+})
+
+let evens = names.filter(function(element){
+    return element.length % 2 == 0; 
+});
+console.log(evens);
+
+let removedElement = names.splice(1,1);
+console.log(removedElement);
+
+function myFunction() {
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+}
+myFunction();
+
+var i = 0
+while (i < 10){
+    console.log(i)
+    i++
+}
+var i = 0
+while (true){
+    console.log(i)
+    i++
+    if (i == 5 ){
+        break
+    }
+}
+console.log("BREAK");
+
+//Loop through an array using a while loop
+var arr = ["tim", "joe", "bill"]
+var i = 0
+while (i < arr.length){
+    console.log(arr[i])
+    i++
+}
+
+var arr = []
+var i = 0
+while (i < 20){
+    arr.push(i)
+    i++
+}
+console.log(arr);
